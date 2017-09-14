964fb020ca97d2f9c3ecd771eb3d791f6c3e9d71
Justin Fincher <zhtsu47@me.com>
2017-09-14 11:48:44 +0800

    Verify Pin With Random String Converted To Bytes

--- Test/TEST_VALIDATE_PIN.py
+++ Test/TEST_VALIDATE_PIN.py
@@ -14,7 +14,7 @@ class TEST_VALIDATE_PIN(Test):
         print(hexifiedPin)
 
         randomMsg = Helper().getChallengeMsg()
-        print(randomMsg)
+        print("RANDOM MSG = " + str(randomMsg))
         if randomMsg == None:
             print("TEST_VALIDATE_PIN FALSE")
             return False
@@ -22,9 +22,12 @@ class TEST_VALIDATE_PIN(Test):
         mfRes = self.getSelectFileBool(FileLocationType.MF)
         adfRes = self.getSelectFileBool(FileLocationType.ADF)
 
+        bytesRandom = bytes.fromhex(randomMsg)
+        print("ByteRandom = " + str(bytesRandom))
+        print("bytes.fromhex(hexifiedPin) = " + str(bytes.fromhex(hexifiedPin)))
         pyDesInstance = pyDes.triple_des(bytes.fromhex(hexifiedPin), pyDes.ECB, b"\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_NORMAL)
         print("pyDesInstance = " + str(pyDesInstance))
-        pyRes = pyDesInstance.encrypt(randomMsg)
+        pyRes = pyDesInstance.encrypt(bytesRandom)
         print("pyRes = " + str(pyRes))
         print("pyRes.hex() = " + str(pyRes.hex()))
         resDict = Helper().getVerifyPinDict(pyRes.hex())
